bbaa6b03b6e987d93f79de178b6d1c9d0222a2c5
DrBuchkov <pafitisnick@gmail.com>
2024-08-31 18:43:59 +0300

    Refactoring and adding tests for initial set of effects.

--- src/com/lambdaseq/fx/core.clj
+++ src/com/lambdaseq/fx/core.clj
@@ -1,44 +1,153 @@
 (ns com.lambdaseq.fx.core)
 
 (defprotocol IEffect
-  (eval! [this]
+  (-eval! [this]
     "Evaluates the effect and returns the result."))
 
-(defrecord Effect [run]
+(defrecord Effect [effect-type run]
   IEffect
-  (eval! [_] (run)))
+  (-eval! [_] (run)))
 
-(defn <succeed [value]
+(defprotocol IFailure)
+
+(defrecord Failure
+  [type data]
+  IFailure)
+
+(defn effect?
+  "Returns true if the value is an effect."
+  [x]
+  (instance? Effect x))
+
+(defn failure?
+  "Returns true if the value is a failure."
+  [x]
+  (instance? Failure x))
+
+(defn make-effect [type run]
+  (Effect. type run))
+
+(defn make-failure [type err]
+  (Failure. type err))
+
+(defmacro maybe-propagate-failure
+  "If the value is an effect, then returns it, otherwise evaluates the body."
+  [v & body]
+  `(if (failure? ~v)
+     ~v
+     (do ~@body)))
+
+(defn succeed> [value]
   "Creates a successful effect that just returns the value."
-  (Effect. (constantly value)))
+  (make-effect :succeed (constantly value)))
 
-(defn <fail [error]
-  "Creates a failed effect that throws an ex-info error."
-  (Effect. (constantly (throw (ex-info error {})))))
+(defn fail>
+  "Creates a failed effect. Takes a failure or a type and an error to be wrapped in a failure,
+   and returns a new effect."
+  ([failure]
+   (make-effect :fail (constantly failure)))
+  ([type error]
+   (make-effect :fail (constantly (make-failure type error)))))
 
-(defn <map
+(defn map>
   "Maps over the effect. Takes a function f and/or an effect, and returns a new effect."
-  ([f]
-   (fn [effect]
-     (<map f effect)))
-  ([f effect]
-   (Effect. (constantly (f (eval! effect))))))
+  [f effect]
+  (make-effect :map
+    (constantly
+      (let [res# (-eval! effect)]
+        (maybe-propagate-failure res#
+          (f res#))))))
 
-(defn <mapcat
+(defn mapcat>
   "Flat maps over the effect. Takes a function f that returns an effect and/or an effect, and returns a new effect."
-  ([f]
-   (fn [effect]
-     (<mapcat f effect)))
-  ([f effect]
-   (Effect. (constantly (eval! (f (eval! effect)))))))
+  [f effect]
+  (make-effect :mapcat
+    (constantly
+      (let [res# (-eval! effect)]
+        (maybe-propagate-failure res#
+          (let [ret# (f res#)]
+            (if (effect? ret#)
+              (-eval! ret#)
+              (make-failure :mapcat>-result-not-an-effect
+                {:result ret#}))))))))
+
+(defn if>
+  "If the condition is true,
+     then returns the `then` effect,
+     otherwise returns the `else` effect."
+  [cond then> else> effect]
+  (make-effect :if
+    (constantly
+      (let [res (-eval! effect)]
+        (maybe-propagate-failure res
+          (-eval!
+            (if (cond res)
+              (then> res)
+              (else> res))))))))
+
+(defmacro pipeline>>
+  "Creates a pipeline of effects."
+  [& body]
+  `(fn [effect#]
+     (->> effect#
+          ~@body)))
 
 
 (comment
 
-  (->
-    (<succeed 1)
-    (<map inc)
-    (<map inc)
-    (eval!))
+  (->>
+    (fail> :foo {})
+    (map> (fn [v] (println "Executed")
+            (inc v)))
+    (-eval!))
+
+  (->>
+    #_(fail> :foo {})
+    (succeed> 42)
+    (mapcat> (fn [v]
+               (println "Executed")
+               (inc v)))
+    (-eval!))
+
+  (def inc-twice>
+    (pipeline>>
+      (map> inc)
+      (map> inc)))
+
+  (->>
+    (succeed> 1)
+    (inc-twice>)
+    (-eval!))
+
+  (->>
+    (succeed> 1)
+    (mapcat> (fn [v] (succeed> (inc v))))
+    (-eval!))
+
+  (def cond-pipeline>
+    (pipeline>>
+      (if> true?
+        (constantly (do
+                      (print "Then branch")
+                      (succeed> 1)))
+        (constantly (do
+                      (print "Else branch")
+                      (fail> :foo "error"))))
+      (map> inc)))
+
+  (->>
+    (succeed> false)
+    (cond-pipeline>)
+    (-eval!))
+
+  (->>
+    (succeed> true)
+    (cond-pipeline>)
+    (-eval!))
+
+  (->>
+    (fail> :foo {})
+    (cond-pipeline>)
+    (-eval!))
 
   )
---
+++ test/com/lambdaseq/fx/core_test.clj
@@ -0,0 +1,178 @@
+(ns com.lambdaseq.fx.core-test
+  (:require [clojure.test :refer :all]
+            [com.lambdaseq.fx.core :refer :all]))
+
+(deftest make-effect-test
+  (let [run (constantly 1)
+        eff (make-effect :test run)]
+    (testing "Return value of make-effect is a valid effect"
+      (is (effect? eff)))
+    (testing "Effect type is correct"
+      (is (= :test (:effect-type eff))))
+    (testing "Effect run function is correct"
+      (is (= 1 (-eval! eff))))))
+
+(deftest make-failure-test
+  (let [err-data {:a 1}
+        fail (make-failure :test err-data)]
+    (testing "Return value of make-failure is a valid failure"
+      (is (failure? fail)))
+    (testing "Failure type is correct"
+      (is (= :test (:type fail))))
+    (testing "Failure data is correct"
+      (is (= err-data (:data fail))))))
+
+(deftest maybe-propagate-failure-test
+  (let [eff (make-effect :test (constantly 1))
+        fail (make-failure :test {:a 1})
+        res (maybe-propagate-failure eff 1)
+        res-fail (maybe-propagate-failure fail 1)]
+    (testing "Returns value if not a failure"
+      (is (= 1 res)))
+    (testing "Returns failure if a failure"
+      (is (= fail res-fail)))))
+
+(deftest succeed>-test
+  (let [eff (succeed> 1)]
+    (testing "Return value of succeed> is a valid effect"
+      (is (effect? eff)))
+    (testing "Effect type is correct"
+      (is (= :succeed (:effect-type eff))))
+    (testing "Effect run function is correct"
+      (is (= 1 (-eval! eff))))))
+
+(deftest fail>-test
+  (let [err-data {:a 1}
+        failure (make-failure :test err-data)
+        eff (fail> :test err-data)]
+    (testing "Return value of fail> is a valid effect"
+      (is (effect? eff)))
+    (testing "Effect type is correct"
+      (is (= :fail (:effect-type eff))))
+    (testing "Effect run function returns a failure"
+      (let [res (-eval! eff)]
+        (is (failure? res))
+        (is (= :test (:type (-eval! eff))))
+        (is (= err-data (:data (-eval! eff))))))))
+
+(deftest map>-test
+  (testing "map> propagates failure"
+    (let [res (->> (fail> :test {})
+                   (map> (constantly 1))
+                   (-eval!))]
+      (is (failure? res))))
+  (testing "map> run function should not evaluate on failure"
+    (let [res (->> (fail> :test {})
+                   (map> (fn [_]
+                           (print "Should not print")
+                           1))
+                   (-eval!)
+                   (with-out-str))]
+      (is (not= "Should not print" res))))
+  (testing "map> applies function to successful effect's value"
+    (let [res (->> (succeed> 1)
+                   (map> inc)
+                   (-eval!))]
+      (is (= 2 res))))
+  (testing "map> run function should evaluate on success"
+    (let [res (->> (succeed> 1)
+                   (map> (fn [x]
+                           (print "Should print")
+                           (inc x)))
+                   (-eval!)
+                   (with-out-str))]
+      (is (= "Should print" res)))))
+
+(deftest mapcat>-test
+  (testing "mapcat> propagates failure"
+    (let [res (->> (fail> :test {})
+                   (mapcat> (constantly (succeed> 1)))
+                   (-eval!))]
+      (is (failure? res))))
+  (testing "mapcat> run function should not evaluate on failure"
+    (let [res (->> (fail> :test {})
+                   (mapcat> (fn [_]
+                              (print "Should not print")
+                              (succeed> 1)))
+                   (-eval!)
+                   (with-out-str))]
+      (is (not= "Should not print" res))))
+  (testing "mapcat> applies function to successful effect's value"
+    (let [res (->> (succeed> 1)
+                   (mapcat> (fn [x]
+                              (succeed> (inc x))))
+                   (-eval!))]
+      (is (= 2 res))))
+  (testing "mapcat> run function should evaluate on success"
+    (let [res (->> (succeed> 1)
+                   (mapcat> (fn [x]
+                              (print "Should print")
+                              (succeed> (inc x))))
+                   (-eval!)
+                   (with-out-str))]
+      (is (= "Should print" res)))))
+
+(deftest if>-test
+  (testing "if> propagates failure"
+    (let [res (->> (fail> :test {})
+                   (if> true
+                     (constantly (succeed> 1))
+                     (constantly (succeed> 2)))
+                   (-eval!))]
+      (is (failure? res))))
+
+  (testing "if> runs the then branch if condition is true"
+    (let [eff (->> (succeed> true)
+                   (if> true?
+                     (constantly (succeed> 1))
+                     (constantly (succeed> 2))))
+          res (-eval! eff)]
+      (is (= 1 res))))
+
+  (testing "side effects in the `else` branch are not evaluated if condition is true"
+    (let [res (->> (succeed> false)
+                   (if> true?
+                     (fn [_]
+                       (print "Should not print")
+                       (succeed> 1))
+                     (fn [_]
+                       (print "Should print")
+                       (succeed> 2)))
+                   (-eval!)
+                   (with-out-str))]
+      (is (= "Should print" res))))
+
+  (testing "if> runs the else branch if condition is true"
+    (let [eff (->> (succeed> false)
+                   (if> true?
+                     (fn [_]
+                       (print "Should not print")
+                       (succeed> 1))
+                     (fn [_]
+                       (print "Should print")
+                       (succeed> 2))))
+          res (-eval! eff)]
+      (is (= 2 res))))
+  (testing "side effects in the `then` branch are not evaluated if condition is false"
+    (let [res (->> (succeed> false)
+                   (if> true?
+                     (fn [_]
+                       (print "Should not print")
+                       (succeed> 1))
+                     (fn [_]
+                       (print "Should print")
+                       (succeed> 2)))
+                   (-eval!)
+                   (with-out-str))]
+      (is (= "Should print" res)))))
+
+(deftest pipeline>-test
+  (testing "Can create a pipeline of effects"
+    (let [inc-twice>
+          (pipeline>>
+            (map> inc)
+            (map> inc))
+          res (->> (succeed> 1)
+                   (inc-twice>)
+                   (-eval!))]
+      (is (= 3 res)))))
